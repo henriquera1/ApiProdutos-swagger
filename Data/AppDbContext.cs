1fd3c333324bc98b54d88643f81a6a2277db97be
henriquera1 <henriquegarcia0401@gmail.com>
2021-08-01 12:04:40 -0300

    Initial commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiProdutos.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    
+        public DbSet<Produto> Produtos { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Produto>()
+                .Property(p => p.Nome)
+                    .HasMaxLength(80);
+            
+            modelBuilder.Entity<Produto>()
+                .Property(p => p.Preco)
+                    .HasPrecision(10, 2);
+
+            modelBuilder.Entity<Produto>()
+                .HasData(
+                    new Produto {Id= 1, Nome = "Laranja", Descricao = "Frutas frescas", Preco = 4.50M, Estoque = 10},
+                    new Produto {Id= 2, Nome = "Abacaxi", Descricao = "Frutas frescas", Preco = 6.00M, Estoque = 10},
+                    new Produto {Id= 3, Nome = "Uva", Descricao = "Frutas frescas", Preco = 2.50M, Estoque = 15},
+                    new Produto {Id= 4, Nome = "Melancia", Descricao = "Frutas frescas", Preco = 5.50M, Estoque = 5}
+                                       
+                );
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+        
+    }
+}
---
+++ Migrations/20210722025120_initial.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ApiProdutos.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(type: "varchar(80)", maxLength: 80, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Descricao = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Preco = table.Column<decimal>(type: "decimal(10,2)", precision: 10, scale: 2, nullable: false),
+                    Estoque = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "Produtos",
+                columns: new[] { "Id", "Descricao", "Estoque", "Nome", "Preco" },
+                values: new object[,]
+                {
+                    { 1, "Frutas frescas", 10, "Laranja", 4.50m },
+                    { 2, "Frutas frescas", 10, "Abacaxi", 6.00m },
+                    { 3, "Frutas frescas", 15, "Uva", 2.50m },
+                    { 4, "Frutas frescas", 5, "Melancia", 5.50m }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+        }
+    }
+}
